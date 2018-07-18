6d25b8e792a1afcb8449061aa6ca4bb5a3172672
245967906 <245967906@qq.com>
2018-07-18 11:26:48 +0800

    使用better-scroll实现列表拖动

--- src/views/city/components/List.vue
+++ src/views/city/components/List.vue
@@ -1,39 +1,48 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">深圳</div>
+  <div class="list wrapper" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">深圳</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">深圳</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">上海</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">深圳</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">巴彦淖尔</div>
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">巴彦淖尔</div>
+        </div>
       </div>
-    </div>
+      </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
 }
 </script>
 
@@ -47,7 +56,7 @@ export default {
     right 0
     bottom 0
     .title
-      line-height .44rem
+      line-height .54rem
       background #eee
       padding-left .2rem
       color #666
