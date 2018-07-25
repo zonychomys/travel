934fd8f9d527fa921f85b3b860d5ca09e9fcbfdd
245967906 <245967906@qq.com>
2018-07-25 19:42:19 +0800

    使用递归组件实现详情页列表

---
+++ src/views/detail/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        <span>{{ item.title }}</span>
+      </div>
+      <div class="item-children" v-if="item.children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height .8rem
+    font-size .32rem
+    padding 0 .6rem
+    .item-title-icon
+      display: inline-block;
+      position: absolute;
+      width: .36rem;
+      height: .36rem;
+      top: .20rem;
+      left: .2rem;
+      background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+      margin-right: .1rem;
+      background-size: .4rem 3rem;
+  .item-children
+    padding 0 .2rem
+</style>
